a57b46799f1225030c6cdf4c4ebc030c28e1ab40
Jihong Min <hurryman2212@gmail.com>
2022-12-18 10:07:19 +0900

    Add nonblock support for all tests and module

--- source/common/ivshmem.c
+++ source/common/ivshmem.c
@@ -1,6 +1,9 @@
+#define _GNU_SOURCE
+
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <unistd.h>
 
@@ -9,43 +12,37 @@
 #include "ivshmem.h"
 
 void intr_wait(int fd, int busy_waiting, uint16_t src_port, int debug) {
-  if (busy_waiting) {
-    while (ioctl(fd, IOCTL_WAIT, src_port) < 0)
-      if (errno != EAGAIN)
-        perror("ioctl(IOCTL_WAIT)");
-      else if (debug)
-        fprintf(stderr, "ioctl(IOCTL_WAIT): EAGAIN\n");
-  } else {
-    if (ioctl(fd, IOCTL_WAIT, src_port))
-      perror("ioctl(IOCTL_WAIT)");
+  do
+    if (!ioctl(fd, IOCTL_WAIT, src_port))
+      return;
+  while ((busy_waiting && (errno == EAGAIN)) || (errno == EINTR));
+  if (errno) {
+    perror("ioctl(IOCTL_WAIT)");
+    exit(EXIT_FAILURE);
   }
 }
 void intr_notify(int fd, int busy_waiting, uint16_t dest_ivposition,
                  uint16_t dest_port, int debug) {
-  if (busy_waiting) {
-    while (ioctl(fd, IOCTL_RING,
-                 IVSHMEM_DOORBELL_MSG(dest_ivposition, dest_port)) < 0)
-      if (errno != EAGAIN)
-        perror("ioctl(IOCTL_RING)");
-      else if (debug)
-        fprintf(stderr, "ioctl(IOCTL_RING): EAGAIN\n");
-  } else {
-    if (ioctl(fd, IOCTL_RING, IVSHMEM_DOORBELL_MSG(dest_ivposition, dest_port)))
-      perror("ioctl(IOCTL_RING)");
+
+  do
+    if (!ioctl(fd, IOCTL_RING,
+               IVSHMEM_DOORBELL_MSG(dest_ivposition, dest_port)))
+      return;
+  while ((busy_waiting && (errno == EAGAIN)) || (errno == EINTR));
+  if (errno) {
+    perror("ioctl(IOCTL_RING)");
+    exit(EXIT_FAILURE);
   }
 }
 
 void uio_wait(int fd, int busy_waiting, int debug) {
   uint32_t dump;
-
-  if (busy_waiting) {
-    while (read(fd, &dump, sizeof(uint32_t)) != sizeof(uint32_t))
-      if (errno != EAGAIN)
-        perror("read()");
-      else if (debug)
-        fprintf(stderr, "read(): EAGAIN\n");
-  } else {
-    if (read(fd, &dump, sizeof(uint32_t)) != sizeof(uint32_t))
-      perror("read()");
+  do
+    if (read(fd, &dump, sizeof(uint32_t)) == sizeof(uint32_t))
+      return;
+  while ((busy_waiting && (errno == EAGAIN)) || (errno == EINTR));
+  if (errno) {
+    perror("read()");
+    exit(EXIT_FAILURE);
   }
 }
