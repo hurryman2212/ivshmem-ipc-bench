912cee5acfd207a443811d9f9a1f4d07daab3d50
jihong <jihong@csap.snu.ac.kr>
2023-05-05 04:32:32 +0900

    uio: Split into different submodule

--- reload-uio.sh
+++ reload-uio.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sudo modprobe -r uio_ivshmem
 sudo modprobe -r kvmfr
+sudo modprobe -r uio_ivshmem
 sudo modprobe -r usernet_ivshmem
 sudo modprobe uio_ivshmem
 sudo chown $USER:$USER /dev/uio* /sys/class/uio/uio0/device/resource2_wc /sys/module/uio_ivshmem/drivers/pci\:uio_ivshmem/*/resource2_wc
--- reload-usernet_ivshmem.sh
+++ reload-usernet_ivshmem.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sudo modprobe -r usernet_ivshmem
-sudo modprobe -r uio_ivshmem
 sudo modprobe -r kvmfr
+sudo modprobe -r uio_ivshmem
+sudo modprobe -r usernet_ivshmem
 sudo modprobe usernet_ivshmem
 sudo chown $USER:$USER /dev/usernet_ivshmem* /sys/module/usernet_ivshmem/drivers/pci\:usernet_ivshmem/*/resource2_wc
